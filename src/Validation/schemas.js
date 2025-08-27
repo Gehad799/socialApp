1110d8698800151543bc974ab2c7ca0b8912dff0
Gehad <gehad.alaa@sarmad.sa>
2025-08-27 18:08:30 +0300

    remove redundant code

--- src/Validation/schemas.js
+++ src/Validation/schemas.js
@@ -1,10 +1,9 @@
 import * as z from "zod";
 const imageValidation = z
   .any()
-  .refine(
-    (files) => !files || !files[0] || files[0].size <= 4 * 1024 * 1024,
-    { message: "Image size must be less than 4MB" }
-  )
+  .refine((files) => !files || !files[0] || files[0].size <= 4 * 1024 * 1024, {
+    message: "Image size must be less than 4MB",
+  })
   .refine(
     (files) =>
       !files ||
@@ -14,31 +13,23 @@ const imageValidation = z
   )
   .optional();
 
+const passwordValidation = z
+  .string({ message: "password is required" })
+  .min(8, {
+    message: "password must be at least 8 characters",
+  })
+  .regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/, {
+    message:
+      "password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+  });
 export const registerSchema = z
   .object({
     email: z.email({ message: "Invalid email address" }),
     name: z
       .string({ message: "name is required" })
       .min(2, { message: "name must be at least 2 characters" }),
-    password: z
-      .string({ message: "password is required" })
-      .min(8, {
-        message: "password must be at least 8 characters",
-      })
-      .regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/, {
-        message:
-          "password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
-      }),
-    rePassword: z
-      .string({ message: "password is required" })
-      .min(8, {
-        message: "password must be at least 8 characters",
-      })
-      .regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/, {
-        message:
-          "password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
-      }),
-
+    password: passwordValidation,
+    rePassword: passwordValidation,
     dateOfBirth: z.string().regex(/^\d{2}-\d{2}-\d{4}$/, {
       message: "Invalid date format, must be YYYY-MM-DD",
     }),
@@ -53,15 +44,7 @@ export const registerSchema = z
 
 export const loginSchema = z.object({
   email: z.email({ message: "Invalid email address" }),
-  password: z
-    .string({ message: "password is required" })
-    .min(8, {
-      message: "password must be at least 8 characters",
-    })
-    .regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/, {
-      message:
-        "password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
-    }),
+  password: passwordValidation,
 });
 
 export const postSchema = z.object({
@@ -76,24 +59,8 @@ export const commentSchema = z.object({
 });
 
 export const changePasswordSchema = z.object({
-  password: z
-    .string({ message: "password is required" })
-    .min(8, {
-      message: "password must be at least 8 characters",
-    })
-    .regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/, {
-      message:
-        "password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
-    }),
-  newPassword: z
-    .string({ message: "password is required" })
-    .min(8, {
-      message: "password must be at least 8 characters",
-    })
-    .regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/, {
-      message:
-        "password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
-    }),
+  password: passwordValidation,
+  newPassword: passwordValidation,
 });
 
 export const editImageSchema = z.object({
